7e30635ca90b1711082b4c28cd87fba5fb9227cb
carlitosft <carlostttt2023gmail.com>
2025-06-10 17:54:17 -0300

    php

---
+++ login.php
@@ -0,0 +1,24 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] == "POST"){
+    $cantidad = $_POST["cantidad"];
+    switch($cantidad){
+        case "1":
+            
+            break;
+        case "2":
+            
+            break;
+        case "3":
+            
+            break;
+        case "4":
+            
+            break;
+        case "5":
+            
+            break;
+    }
+}else{
+    echo "error";
+}
+?>
